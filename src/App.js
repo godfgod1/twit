46fca0a05d5b9400a21aed8c7eb82daa14193105
godfgod1 <godfgod1@naver.com>
2022-04-07 20:38:27 +0900

    set-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Auth from "./routes/Auth"
+import Home from "./routes/Home"
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return(
+    <Router>
+      <Routes>
+      { isLoggedIn ? 
+        <Route  exact={true}  path="/" element={<Home/>}/>
+        :
+        <Route  exact={true}  path="/" element={<Auth/>}/>
+        }
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
+
